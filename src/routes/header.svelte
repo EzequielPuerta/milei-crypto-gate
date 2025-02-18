da46282ed87a3d0792a1d22f3444e5a9e9c1bc12
Ezequiel Puerta <eze19.2009@gmail.com>
2025-02-18 17:44:47 -0300

    meme removed

--- src/routes/header.svelte
+++ src/routes/header.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import * as config from '$lib/config'
     import { Navbar, NavBrand } from 'flowbite-svelte';
+    import { Photo } from '../lib/components/Photo';
 </script>
 
 <Navbar fluid={true}>
     <NavBrand href="/">
-        <img src="https://pbs.twimg.com/media/Gj06IyBXIAAMrPC.jpg" class="me-3 h-6 sm:h-9" alt="Milei-Hermosa mañana verdad?" />
+        <img src={Photo.JavierMilei} class="me-3 h-6 sm:h-9" alt="Milei" />
         <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{config.title}</span>
     </NavBrand>
 </Navbar>
