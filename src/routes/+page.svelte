10b773dde00a73c51b888c7b3e8cd22261fa652d
Ezequiel Puerta <eze19.2009@gmail.com>
2025-02-18 19:49:43 -0300

    viale's reply

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -847,6 +847,7 @@
         <p class="timeline-item-p">
             <Badge rounded large color="dark">Javier Milei</Badge>
             <Badge rounded large color="dark">Jonatan Viale</Badge>
+            <Badge rounded large color="dark">Santiago Caputo</Badge>
         </p>
 
         <p class="timeline-item-p">
@@ -865,6 +866,35 @@
             href="https://www.pagina12.com.ar/804765-ordenes-al-ensobrado"
             target="_blank">
             Fuente: Página12
+        </Button>
+    </TimelineItem>
+
+    <TimelineItem title="'No reaccioné como tendría que haber reaccionado': Jonatan Viale sobre la entrevista con Milei" date="18 de febrero, 2025" classTime="text-md">
+        <svelte:fragment slot="icon">
+            <TimelineAvatar avatar={CustomAvatar.JonatanViale} />
+        </svelte:fragment>
+        <p class="timeline-item-p">
+            <Badge rounded large color="dark">Javier Milei</Badge>
+            <Badge rounded large color="dark">Jonatan Viale</Badge>
+            <Badge rounded large color="dark">Santiago Caputo</Badge>
+        </p>
+
+        <p class="timeline-item-p">
+            El periodista Jonatan Viale decidió dar explicaciones sobre la entrevista que le realizó este lunes a Javier Milei, de la que trascendió un fragmento editado de una intervención del asesor Santiago Caputo para interrumpir una pregunta. "No reaccioné como tendría que haber reaccionado", dijo sobre este martes el conductor de TN en una aparición en su programa radial del que se había ausentado.
+        </p>
+        <p class="timeline-item-p">
+            “Para los que tengan dudas, les voy a contar lo que pasó. Pero solo les quiero adelantar que nunca jamás en la vida agarré un mango de este ni de ningún Gobierno. No lo quiero. No me interesa. Tuve mil oportunidades. Rechacé todas. De todas maneras, corresponde que dé una explicación tranquila y larga en el lugar en el que pasó. Esta situación es fea", dijo Viale en Radio Rivadavia.
+        </p>
+
+        <p class="timeline-item-p">
+            El periodista se anticipó a lo que responderá en su programa de televisión en TN, donde fue la entrevista con Milei. “Esta noche en Todo Noticias voy a hacer un pequeño descargo respecto de lo que pasó. Voy a explicar la intervención de Caputo. Voy a contar todo. Qué hice, qué no hice, qué me falto hacer, la filtración del video. Voy a explicárselo a la gente que nos viene mirando hace muchos años", anticipó.
+        </p>
+
+        <Button
+            color="alternative"
+            href="https://www.clarin.com/politica/reaccione-reaccionado-jonatan-viale-hablo-momento-santiago-caputo-interrumpio-entrevista-javier-milei_0_yCSRZ2ykZh.html"
+            target="_blank">
+            Fuente: Clarín
         </Button>
     </TimelineItem>
 
