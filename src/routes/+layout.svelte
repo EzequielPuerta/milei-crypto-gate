f163d0ae7585923eaf5ca91b15d9d5470f9d03fe
Ezequiel Puerta <eze19.2009@gmail.com>
2025-02-19 13:09:36 -0300

    scroll button style changed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,7 +28,7 @@
         <Button
             class="scroll-bottom-btn"
             pill={true}
-            color="dark"
+            color="alternative"
             on:click={scrollToBottom}
         >
             <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
